c8c9d34c76811176d6c719527a69ec456005c0d2
Oliver Benns <oliverbenns@gmail.com>
2015-12-15 21:25:12 +1100

    Add basic drawing functionality to test canvas is functional and behaving correctly

--- app/index.js
+++ app/index.js
@@ -1,4 +1,25 @@
 const canvas = document.getElementById('pong');
+const context = canvas.getContext('2d');
 
-console.log('canvas', canvas);
+const COLORS = {
+	BACKGROUND: '#000',
+	PROPS: '#FFF',
+};
 
+context.fillStyle = COLORS.BACKGROUND;
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+const Player = function() {};
+
+Player.prototype.draw = function(positionX, positionY) {
+	context.beginPath();
+	context.rect(positionX, positionY, 10, 20);
+	context.fillStyle = COLORS.PROPS;
+	context.fill();
+}
+
+const playerOne = new Player();
+const playerTwo = new Player();
+
+playerOne.draw(5, 50);
+playerTwo.draw(285, 50);
