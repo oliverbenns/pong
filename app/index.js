acca7fc3028a539ab72c152f4327dec4c537ff87
Oliver Benns <oliverbenns@gmail.com>
2015-12-17 22:44:23 +1100

    Add jslint npm task to keep code in order. Fix current code

--- app/index.js
+++ app/index.js
@@ -2,21 +2,21 @@ const canvas = document.getElementById('pong');
 const context = canvas.getContext('2d');
 
 const COLORS = {
-	BACKGROUND: '#000',
-	PROPS: '#FFF',
+  BACKGROUND: '#000',
+  PROPS: '#FFF',
 };
 
 context.fillStyle = COLORS.BACKGROUND;
 context.fillRect(0, 0, canvas.width, canvas.height);
 
-const Player = function() {};
+const Player = function player() {};
 
-Player.prototype.draw = function(positionX, positionY) {
-	context.beginPath();
-	context.rect(positionX, positionY, 10, 20);
-	context.fillStyle = COLORS.PROPS;
-	context.fill();
-}
+Player.prototype.draw = function draw(positionX, positionY) {
+  context.beginPath();
+  context.rect(positionX, positionY, 10, 20);
+  context.fillStyle = COLORS.PROPS;
+  context.fill();
+};
 
 const playerOne = new Player();
 const playerTwo = new Player();
