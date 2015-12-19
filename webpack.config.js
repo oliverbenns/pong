587f270587a96a7c57dd63e356ab77c7f53050b6
Oliver Benns <oliverbenns@gmail.com>
2015-12-19 17:15:00 +1100

    Update web pack to using spaces

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,34 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-	context: __dirname + "/app",
-	entry: "./index.js",
+  context: __dirname + '/app',
+  entry: './index.js',
 
-	output: {
-		path: __dirname + '/bin',
-		publicPath: "/",
-		filename: 'bundle.js',
-	},
+  output: {
+    path: __dirname + '/bin',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
 
-	stats: {
-		colors: true,
-		progress: true,
-	},
+  stats: {
+    colors: true,
+    progress: true,
+  },
 
-	resolve: {
-		extensions: ['', '.webpack.js', '.js'],
-	},
+  resolve: {
+    extensions: ['', '.webpack.js', '.js'],
+  },
 
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				query: {
-					presets: ['es2015'],
-				},
-				loader: 'babel',
-			},
-		],
-	},
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015'],
+        },
+        loader: 'babel',
+      },
+    ],
+  },
 };
