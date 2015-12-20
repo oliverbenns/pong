db376148b247cc7327fa6a44139ae87887e85dfa
Oliver Benns <oliverbenns@gmail.com>
2015-12-20 17:12:58 +1100

    Add clear method to canvas

--- app/canvas.js
+++ app/canvas.js
@@ -6,4 +6,11 @@ canvas.context = canvas.getContext('2d');
 canvas.context.fillStyle = constants.COLORS.BACKGROUND;
 canvas.context.fillRect(0, 0, canvas.width, canvas.height);
 
+canvas.clear = function() {
+  canvas.context.beginPath();
+  canvas.context.rect(0, 0, canvas.width, canvas.height);
+  canvas.context.fillStyle = constants.COLORS.BACKGROUND;
+  canvas.context.fill();
+};
+
 export default canvas;
