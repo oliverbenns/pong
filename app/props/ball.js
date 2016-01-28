8f55895ce28232974d8b4bd44c03ee0f4d3021db
Oliver Benns <oliverbenns@gmail.com>
2016-01-28 13:46:25 +1100

    Move ball in a direction, stopping when hitting canvas edge.

--- app/props/ball.js
+++ app/props/ball.js
@@ -1,6 +1,25 @@
 import Prop from 'props/prop';
 import canvas from 'canvas';
 
+const coords = {
+  northEast: {
+    x: 1,
+    y: -1,
+  },
+  southEast: {
+    x: 1,
+    y: 1,
+  },
+  southWest: {
+    x: -1,
+    y: 1,
+  },
+  northWest: {
+    x: -1,
+    y: -1,
+  },
+};
+
 export default class Ball extends Prop {
   constructor() {
     const width = 10;
@@ -11,9 +30,25 @@ export default class Ball extends Prop {
     super(x, y, width, height);
   }
 
+  isOutOfBounds() {
+    // This will be moved into a collision detection lib at some point.
+    return (
+      this.positionY === canvas.height ||
+      this.positionY === 0 ||
+      this.positionX === canvas.width ||
+      this.positionX === 0
+    );
+  }
+
   fire() {
-    setInterval(() => {
-      this.move(1, 1);
+    const direction = coords.northWest;
+
+    const move = setInterval(() => {
+      this.move(direction.x, direction.y);
+      if (this.isOutOfBounds()) {
+        console.log('Stopping ball, out of bounds');
+        clearInterval(move);
+      }
     }, 10);
   }
 }
