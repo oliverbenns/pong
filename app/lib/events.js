1e4452ef2522cf0ba451b6699b3f5ed694984176
oliverbenns <oliverbenns@gmail.com>
2016-02-09 19:31:01 +1100

    Lint & ES6ify events

--- app/lib/events.js
+++ app/lib/events.js
@@ -1,36 +1,34 @@
 // https://davidwalsh.name/pubsub-javascript
-var events = (function(){
-  var topics = {};
+const events = (() => {
+  const topics = {};
   const { hasOwnProperty } = topics;
 
   return {
-    subscribe: function(topic, listener) {
+    subscribe(topic, listener) {
       // Create the topic's object if not yet created
       if (!hasOwnProperty.call(topics, topic)) {
         topics[topic] = [];
       }
 
       // Add the listener to queue
-      var index = topics[topic].push(listener) -1;
+      const index = topics[topic].push(listener) - 1;
 
       // Provide handle back for removal of topic
       return {
-        remove: function() {
+        remove() {
           delete topics[topic][index];
-        }
+        },
       };
     },
-    publish: function(topic, info) {
+    publish(topic, info) {
       // If the topic doesn't exist, or there's no listeners in queue, just leave
       if (!hasOwnProperty.call(topics, topic)) {
         return;
       }
 
       // Cycle through topics queue, fire!
-      topics[topic].forEach(function(item) {
-          item(info != undefined ? info : {});
-      });
-    }
+      topics[topic].forEach((item) => item(info !== undefined ? info : {}));
+    },
   };
 })();
 
