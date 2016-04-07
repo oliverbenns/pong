71e8730e33a91aa52e8e7712db5585b8ab352832
oliverbenns <oliverbenns@gmail.com>
2016-04-07 20:58:56 +1000

    Use more simple AABB algorithm

--- app/lib/collision.js
+++ app/lib/collision.js
@@ -30,25 +30,14 @@ export default {
 
     const AX = prop1.x + prop1.width;
     const AY = prop1.y + prop1.height;
-
     const BX = prop2.x + prop2.width;
     const BY = prop2.y + prop2.height;
 
-    const onSameYAxis = (AY >= prop2.y && AY <= BY) || (prop1.y >= prop2.y && prop1.y <= BY);
-    const onSameXAxis = (AX >= prop2.x && AX <= BX) || (prop1.x >= prop2.x && prop1.x <= BX);
-
-    return onSameYAxis && onSameXAxis;
-
-    // Not sure if this is more easy to reason about. Leaving here for now.
-    // return !(
-    //   AX < prop2.x ||
-    //   BX < prop1.x ||
-    //   AY < prop2.y ||
-    //   BY < prop1.y
-    // );
+    return !(
+      AX < prop2.x ||
+      BX < prop1.x ||
+      AY < prop2.y ||
+      BY < prop1.y
+    );
   },
-
-  // Detect when hit left / right hand canvas side
-  // Detect when hit top / bottom canvas side
-  // Detect when object touches other object.
 };
