c293fcf149e76518ad79720889efd226fd3de54e
oliverbenns <oliverbenns@gmail.com>
2016-04-12 20:58:00 +1000

    Fix net rendering that always left space at the bottom of render iteration

--- app/props/net.js
+++ app/props/net.js
@@ -6,18 +6,19 @@ export default class Net extends Prop {
     super(null, null, 8, canvas.height);
   }
 
-  // Players can only move Y axis.
   render() {
+    const iterationHeight = this.height / 20;
+    const lineHeight = iterationHeight * 0.666;
+    const spacing = (iterationHeight * 0.333) / 2;
+
+    const x = (canvas.width / 2) - (this.width / 2);
     let y = 0;
 
     while (y < this.height) {
-      const line = new Prop();
-      line.width = this.width;
-      line.height = 20;
+      const line = new Prop(x, y + spacing, this.width, lineHeight);
 
-      line.render((canvas.width / 2) - (this.width / 2), y);
-
-      y += 40;
+      line.render();
+      y += iterationHeight;
     }
   }
 }
