1333ca231a51f4bef3d7cd8b394362f64aa812e2
oliverbenns <oliverbenns@gmail.com>
2016-04-13 20:57:28 +1000

    Add speed to ai, and fix calculation of where to move to stop lagging

--- app/props/ai.js
+++ app/props/ai.js
@@ -5,12 +5,21 @@ export default class Ai extends Paddle {
   constructor(ball) {
     super(canvas.width - 15, ball.y);
     this.ball = ball;
+    this.speed = 3;
   }
 
   move() {
     const centerBallPoint = this.ball.y - (this.ball.height / 2);
 
-    // @TODO: Need to implement speed here.
-    return super.move(this.y < centerBallPoint ? 1 : -1);
+    if (this.y < centerBallPoint) {
+      console.log('paddle is higher than ball');
+      const distanceToBall = centerBallPoint - this.y;
+      super.move(distanceToBall < this.speed ? distanceToBall : this.speed);
+    }
+
+    // Ai is lower than ball
+    const distanceToBall = centerBallPoint - this.y;
+    console.log('paddle is lower than ball');
+    super.move(distanceToBall < this.speed ? distanceToBall : this.speed);
   }
 }
