48278552aee15cd55fd9f348539ae6dc2b0e81ae
oliverbenns <oliverbenns@gmail.com>
2016-04-14 20:36:42 +1000

    Fix button vertical text alignment calculation

--- app/props/button.js
+++ app/props/button.js
@@ -12,12 +12,14 @@ export default class Button extends Clickable {
   render() {
     const fontSize = 48;
     const fontX = canvas.width / 2;
+    const fontY = this.y + (this.height / 2);
 
     super.render();
 
     canvas.context.font = `${fontSize}px Helvetica`;
     canvas.context.textAlign = 'center';
     canvas.context.fillStyle = constants.COLORS.PROPS;
-    canvas.context.fillText(this.text, fontX, this.y + (this.height / 2 + 12));
+    canvas.context.textBaseline = 'middle';
+    canvas.context.fillText(this.text, fontX, fontY);
   }
 }
